ba6310db0af06124030671b3382b1086901809c5
MUKAIBI <info@mukaibi.com>
2018-07-01 09:04:57 +0900

    P203

--- script.js
+++ script.js
@@ -1,18 +1,6 @@
-Vue.component('comp-child', {
-    template: '<button v-on:click="handleClick">イベント発火</button>',
-    methods: {
-        handleClick: function() {
-            this.$emit('childs-event')
-        }
-    }
-})
-
-
 new Vue({
     el: '#app',
-    methods: {
-        parentsMethod: function() {
-            alert('イベントをキャッチ！')
-        }
+    data: {
+        count: 0
     }
 })
